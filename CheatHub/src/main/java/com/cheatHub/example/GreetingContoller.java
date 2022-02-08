10644b5bd89a815587a849d0864abeb3d85b6d99
jm-real-2018 <84847153+jm-real-2018@users.noreply.github.com>
2022-02-08 14:40:20 +0100

    Añadimos controladores y primeras versiones de CheatHub

--- CheatHub/src/main/java/com/cheatHub/example/GreetingContoller.java
+++ CheatHub/src/main/java/com/cheatHub/example/GreetingContoller.java
@@ -3,35 +3,50 @@ package com.cheatHub.example;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingContoller {
 	
-	@GetMapping("/condicional")
-	public String greeting(Model model) {
+	private Object filtro;
+
+	@RequestMapping("/search")
+	public String greeting(Model model, @RequestParam String userName,String filtro) {
 		/*Los atributos de esta función corresponden a:
 		 1: "name": que corresponde al nombre de la plantilla del fichero html. ->index.html->{{name}}
 		 2: "wordl": Lo que sustituimos en dicha palabra
 		 */
-		model.addAttribute("name","world");
+		model.addAttribute("name",userName);
+		if(filtro!=null) {
+			model.addAttribute("fill",filtro);
+		}
+		else {
+			model.addAttribute("fill","ninguno");
+		}
+		
 		/*Devolvemos como cadena de texto el nombre del archivo html en el que se encuentra la plantilla
 		que se debe de encontrar en la carpeta de templates.
 		*/
-		return "index";
+		return "principal";
 	}
 	
-
-	@GetMapping("/")
-	public String greetingCond(Model model) {
-		/*Los atributos de esta función corresponden a: 
+	@RequestMapping("/category")
+	public String greetingCategory(Model model, @RequestParam String boton) {
+		/*Los atributos de esta función corresponden a:
 		 1: "name": que corresponde al nombre de la plantilla del fichero html. ->index.html->{{name}}
 		 2: "wordl": Lo que sustituimos en dicha palabra
 		 */
-		model.addAttribute("name","world");
+			model.addAttribute("fill",boton);
+		
 		/*Devolvemos como cadena de texto el nombre del archivo html en el que se encuentra la plantilla
-		 * que se debe de encontrar en la carpeta de templates
+		que se debe de encontrar en la carpeta de templates.
 		*/
-		model.addAttribute("hello", true);
+		return "category";
+	}
+
+	@GetMapping("/")
+	public String greetingCond(Model model) {
 		
 		return "index";
 	}
